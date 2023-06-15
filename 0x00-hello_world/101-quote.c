741bc267ea95f1e583fafda1e7bf04dc956b6db9
maxmaster55 <youssefalkashef000@gmail.com>
2023-06-15 20:31:22 +0300

    dsaa

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    int i = 0;
+    while (str[i] == '\0')
+    {
+        putchar(str[i]);
+        i += 1;
+    }
+
+    return (1);
+}
