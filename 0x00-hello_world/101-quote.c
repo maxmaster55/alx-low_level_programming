30f0e6d3aa705de8dca1206074dad9e6a1d0591c
maxmaster <yousseflkashef000@gmail.com>
2023-06-15 10:36:44 -0700

    auto commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,13 +6,14 @@
  */
 int main(void)
 {
-    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-    int i = 0;
-    while (str[i] == '\0')
-    {
-        putchar(str[i]);
-        i += 1;
-    }
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i = 0;
 
-    return (1);
+	while (str[i] == '\0')
+	{
+		putchar(str[i]);
+		i += 1;
+	}
+
+	return (1);
 }
