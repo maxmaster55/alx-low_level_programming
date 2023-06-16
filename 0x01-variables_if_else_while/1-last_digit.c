b023641e54c61385046d13cd79b75632ffea958d
maxmaster <yousseflkashef000@gmail.com>
2023-06-16 05:41:14 -0700

    auto commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@
 int main(void)
 {
 	int n;
-
+	int x;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int x = n % 10;
+	x = n % 10;
 
 	if (x == 0)
 		printf("Last digit of %d is %d and is 0\n", n, x);
